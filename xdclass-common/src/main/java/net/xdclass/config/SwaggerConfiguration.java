58600c559bf83f67842035b2a7d2dfb3da427dd3
j1anyi <pp517462915@gmail.com>
2023-09-06 20:31:53 +0200

    swagger-ui add request head and response message

--- xdclass-common/src/main/java/net/xdclass/config/SwaggerConfiguration.java
+++ xdclass-common/src/main/java/net/xdclass/config/SwaggerConfiguration.java
@@ -2,15 +2,17 @@ package net.xdclass.config;
 
 import lombok.Data;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.*;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Data
@@ -30,6 +32,25 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("net.xdclass"))
                 //正则匹配请求路径，并分配至当前分组
                 .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .globalRequestParameters(getGlobalRequestParameters())
+                .globalResponses(HttpMethod.GET, getGlobalResponseMessage())
+                .globalResponses(HttpMethod.POST, getGlobalResponseMessage());
+    }
+
+    @Bean
+    public Docket adminApiDoc() {
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("管理员中心API接口文档")
+                .pathMapping("/")
+                .apiInfo(webApiInfo())
+                // 开启swagger
+                .enable(true)
+                // 选择哪些接口作为swagger的doc发布
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("net.xdclass"))
+                //正则匹配请求路径，并分配至当前分组
+                .paths(PathSelectors.ant("/admin/**"))
                 .build();
     }
 
@@ -40,6 +61,43 @@ public class SwaggerConfiguration {
                 .version("v1")
                 .contact(new Contact("j1anyi", "", "J1anYi@github.com"))
                 .build();
+    }
+
+    /**
+     * 生成全局通用参数, 支持配置多个响应参数
+     * @return
+     */
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("token")
+                .description("登录令牌")
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+
+//        parameters.add(new RequestParameterBuilder()
+//                .name("version")
+//                .description("版本号")
+//                .required(true)
+//                .in(ParameterType.HEADER)
+//                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+//                .required(false)
+//                .build());
+        return parameters;
+    }
+
+
+    /**
+     * 生成通用响应信息
+     * @return
+     */
+    private List<Response> getGlobalResponseMessage() {
+        List<Response> responseList = new ArrayList<>();
+        responseList.add(new ResponseBuilder().code("4xx").description("请求错误，根据code和msg检查").build());
+
+        return responseList;
     }
 
 }
